92b8d436ec1316f6503339e52bd452e5befc108b
Utian Ayuba <utianayuba@gmail.com>
2015-12-03 16:40:14 +0800

    Update networking, glance and nova commands and configs

--- compute/liberty_compute.sh
+++ compute/liberty_compute.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-##### Repositories #####
-rm -rf /etc/zypp/repos.d/repo-debug*
-rm -rf /etc/zypp/repos.d/repo-source*
-zypper ar http://download.opensuse.org/repositories/Cloud:/OpenStack:/Liberty/openSUSE_Leap_42.1/Cloud:OpenStack:Liberty.repo
-zypper mr -R --all
-zypper mr -e --all
-zypper ref
-zypper up --skip-interactive
-zypper -n in --no-recommends python-openstackclient
+##### IP Address #####
+yast2
 
 ##### Name Resolution #####
 echo "10.10.10.10 controller" >> /etc/hosts
 echo "10.10.10.30 storage" >> /etc/hosts
+reboot
 
 ##### NTP Service #####
 echo "server 0.opensuse.pool.ntp.org iburst" >> /etc/ntp.conf
@@ -22,8 +16,22 @@ echo "server 3.opensuse.pool.ntp.org iburst" >> /etc/ntp.conf
 systemctl enable ntpd.service
 systemctl start ntpd.service
 
-##### Compute #####
+##### Repositories #####
+zypper rm patterns-openSUSE-minimal_base-conflicts
+zypper ar -f obs://Cloud:OpenStack:Liberty/openSUSE_Leap_42.1 Liberty
+zypper mr -R --all
+zypper mr -e --all
+zypper --gpg-auto-import-keys ref
+zypper -n up --skip-interactive && zypper -n dist-upgrade
+reboot
+
+zypper -n in --no-recommends python-openstackclient
 zypper in --no-recommends http://download.opensuse.org/repositories/Cloud:/Eucalyptus/openSUSE_Leap_42.1/noarch/euca2ools-3.0.4-1.2.noarch.rpm
+
+##### Copy compute config directory to /root and change current dir to compute #####
+cd /root/compute
+
+##### Nova Compute Service #####
 zypper -n in --no-recommends openstack-nova-compute genisoimage qemu-kvm libvirt
 cp etc/nova/nova.conf /etc/nova/nova.conf
 chown root:nova /etc/nova/nova.conf
@@ -32,7 +40,7 @@ cp etc/modules-load.d/nbd.conf /etc/modules-load.d/nbd.conf
 systemctl enable libvirtd.service openstack-nova-compute.service
 systemctl start libvirtd.service openstack-nova-compute.service
 
-##### Neutron #####
+##### Neutron Networking Service #####
 zypper -n in --no-recommends openstack-neutron-linuxbridge-agent ipset
 cp etc/neutron/neutron.conf /etc/neutron/neutron.conf
 chown root:neutron /etc/neutron/neutron.conf
--- controller/liberty_controller.sh
+++ controller/liberty_controller.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-##### Repositories #####
-rm -rf /etc/zypp/repos.d/repo-debug*
-rm -rf /etc/zypp/repos.d/repo-source*
-zypper ar http://download.opensuse.org/repositories/Cloud:/OpenStack:/Liberty/openSUSE_Leap_42.1/Cloud:OpenStack:Liberty.repo
-zypper mr -R --all
-zypper mr -e --all
-zypper ref
-zypper up --skip-interactive
-zypper -n in --no-recommends python-openstackclient
+##### IP Address #####
+cp etc/sysconfig/network/ifcfg-eth0 /etc/sysconfig/network/ifcfg-eth0
+cp etc/sysconfig/network/ifcfg-eth1 /etc/sysconfig/network/ifcfg-eth1
 
 ##### Name Resolution #####
 echo "10.10.10.20 compute" >> /etc/hosts
@@ -22,6 +16,21 @@ echo "server 3.opensuse.pool.ntp.org iburst" >> /etc/ntp.conf
 systemctl enable ntpd.service
 systemctl start ntpd.service
 
+##### Repositories #####
+zypper rm patterns-openSUSE-minimal_base-conflicts
+zypper ar -f obs://Cloud:OpenStack:Liberty/openSUSE_Leap_42.1 Liberty
+zypper mr -R --all
+zypper mr -e --all
+zypper --gpg-auto-import-keys ref
+zypper -n up --skip-interactive && zypper -n dist-upgrade
+reboot
+
+zypper -n in --no-recommends python-openstackclient
+zypper in --no-recommends http://download.opensuse.org/repositories/Cloud:/Eucalyptus/openSUSE_Leap_42.1/noarch/euca2ools-3.0.4-1.2.noarch.rpm
+
+##### Copy controller config directory to /root and change current directory to controller #####
+cd /root/controller
+
 ##### MySQL Database Service #####
 zypper -n in --no-recommends mysql-community-server mysql-community-server-client python-PyMySQL
 cp etc/my.cnf.d/mysql_openstack.cnf /etc/my.cnf.d/mysql_openstack.cnf
@@ -33,12 +42,12 @@ mysql -e "DELETE FROM mysql.user WHERE User='';"
 mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-
 ##### RabbitMQ Service #####
 zypper -n in --no-recommends rabbitmq-server
 cp etc/systemd/system/epmd.socket /etc/systemd/system/epmd.socket
 systemctl enable epmd.service
-systemctl start epmd.service
+systemctl restart epmd.service
+sleep 5
 systemctl enable rabbitmq-server.service
 systemctl start rabbitmq-server.service
 rabbitmqctl add_user openstack PASSWORD
@@ -51,6 +60,7 @@ systemctl enable memcached.service
 systemctl start memcached.service
 cp etc/keystone/keystone.conf /etc/keystone/keystone.conf
 su -s /bin/sh -c "keystone-manage db_sync" keystone
+cp etc/sysconfig/apache2 /etc/sysconfig/apache2
 cp etc/apache2/conf.d/wsgi-keystone.conf /etc/apache2/conf.d/wsgi-keystone.conf
 chown -R keystone:keystone /etc/keystone
 a2enmod version
@@ -65,8 +75,6 @@ openstack service create --name keystone --description "OpenStack Identity" iden
 openstack endpoint create --region RegionOne identity public http://controller:5000/v2.0
 openstack endpoint create --region RegionOne identity internal http://controller:5000/v2.0
 openstack endpoint create --region RegionOne identity admin http://controller:35357/v2.0
-
-##### Projects, Users, and Roles #####
 openstack project create --domain default --description "Admin Project" admin
 openstack user create --domain default --password PASSWORD admin
 openstack role create admin
@@ -74,6 +82,9 @@ openstack role add --project admin --user admin admin
 openstack project create --domain default --description "Service Project" service
 openstack role create user
 cp root/admin-openrc.sh /root/admin-openrc.sh
+unset OS_TOKEN OS_URL OS_IDENTITY_API_VERSION
+source /root/admin-openrc.sh 
+openstack token issue
 
 ##### Glance Image Service #####
 mysql -u root -pPASSWORD -e "CREATE DATABASE glance; GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'PASSWORD'; GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'PASSWORD';"
@@ -90,6 +101,7 @@ cp etc/glance/glance-registry.conf /etc/glance/glance-registry.conf
 chown root:glance /etc/glance/glance-api.conf /etc/glance/glance-registry.conf
 systemctl enable openstack-glance-api.service openstack-glance-registry.service
 systemctl start openstack-glance-api.service openstack-glance-registry.service
+glance image-list
 
 ##### Nova Compute Service #####
 mysql -u root -pPASSWORD -e "CREATE DATABASE nova; GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'PASSWORD'; GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'PASSWORD';"
@@ -100,16 +112,18 @@ openstack service create --name nova --description "OpenStack Compute" compute
 openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
 openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
 openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
-zypper in --no-recommends http://download.opensuse.org/repositories/Cloud:/Eucalyptus/openSUSE_Leap_42.1/noarch/euca2ools-3.0.4-1.2.noarch.rpm
 zypper -n in --no-recommends openstack-nova-api openstack-nova-scheduler openstack-nova-cert openstack-nova-conductor openstack-nova-consoleauth openstack-nova-novncproxy python-novaclient iptables
 cp etc/nova/nova.conf /etc/nova/nova.conf
 chown root:nova /etc/nova/nova.conf
 systemctl enable openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
 systemctl start openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service
+nova service-list
+nova endpoints
+nova image-list
 
 ##### Neutron Networking Service #####
 mysql -u root -pPASSWORD -e "CREATE DATABASE neutron; GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY 'PASSWORD'; GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' IDENTIFIED BY 'PASSWORD';"
-source admin-openrc.sh
+source /root/admin-openrc.sh
 openstack user create --domain default --password PASSWORD neutron
 openstack role add --project service --user neutron admin
 openstack service create --name neutron --description "OpenStack Networking" network
@@ -135,3 +149,4 @@ cp srv/www/openstack-dashboard/openstack_dashboard/local/local_settings.py /srv/
 chown wwwrun:www /srv/www/openstack-dashboard/openstack_dashboard/local/local_settings.py
 systemctl enable apache2.service memcached.service
 systemctl restart apache2.service memcached.service
+
